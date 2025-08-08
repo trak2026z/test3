264c86a2f169831b170c03fdbabcfa30913d9603
trak2026z <trak2026z@gmail.com>
2025-08-08 19:46:36 +0200

    feat: add App component rendering EpicPhoto

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { EpicPhoto } from "./components/EpicPhoto";
+
+const App: React.FC< > = () => {
+  return (
+    <main className="p-4">
+      <EpicPhoto />
+    </main>
+  );
+};
+
+export default App;
